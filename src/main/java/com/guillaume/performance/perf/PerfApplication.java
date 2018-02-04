6ceab3060d16a53257c853643bc04587d135ab95
Whazaby <guillaume.ruscassie@gmail.com>
2018-02-04 19:02:41 +0100

    first commit

---
+++ src/main/java/com/guillaume/performance/perf/PerfApplication.java
@@ -0,0 +1,12 @@
+package com.guillaume.performance.perf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfApplication.class, args);
+	}
+}
